e1b4734ae177e3b14ccbcb041720852fe0862b8f
Ejder Yurdakul <ejder.yurdakul@student.ucll.be>
2026-03-28 14:10:55 +0100

    bg image verbeterd, button position

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class ButtonType(Enum):
     START = 2
     HIT = 3
     STAND = 4
+    BACK = 5
 
 TABLE_GREEN = (16, 102, 58)
 
@@ -23,14 +24,19 @@ WOOD_DARK = (101, 51, 15)
 GOLD = (212, 175, 55)
 WHITE = (255, 255, 255)
 
+GAP = 40
+BUTTON_WIDTH = 180
+BUTTON_HEIGHT = 60
+
 class BlackjackGame:
-    def __init__(self, width=800, height=600):
+    def __init__(self, width=1024, height=768):
         pygame.init()
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
         self.state = GameState.MENU
+        self.__initiate_positions()
         self.__initiate_buttons()
         self.__initiate_title()
         self.running = True
@@ -39,29 +45,57 @@ class BlackjackGame:
         self.title_img = pygame.image.load(
             os.path.join("assets", "title.png")
         ).convert_alpha()
-        self.title_rect = self.title_img.get_rect(center=(self.width // 2, 80))
+        self.title_rect = self.title_img.get_rect(center=(self.center_x, self.center_y - 50))
 
+    def __initiate_positions(self):
+        self.center_x = self.width // 2
+        self.center_y = self.height // 2
+        self.pos_y = self.height - BUTTON_HEIGHT - GAP
+
+        self.pos1_x = self.center_x - BUTTON_WIDTH - (GAP // 2)
+        self.pos2_x = self.center_x + (GAP // 2)
+        self.pos4_x = self.center_x - (BUTTON_WIDTH // 2)
+        self.pos3_x = self.pos4_x - BUTTON_WIDTH - GAP
+        self.pos5_x = self.pos4_x + BUTTON_WIDTH + GAP
+    
     def __initiate_buttons(self):
-        self.start_button = Button(ButtonType.START, 300, 150)
-        self.hit_button = Button(ButtonType.HIT, 300, 250)
-        self.stand_button = Button(ButtonType.STAND, 300, 350)
-        self.quit_button = Button(ButtonType.QUIT, 300, 450)
+        self.start_button = Button(ButtonType.START, self.pos1_x, self.pos_y)
+        self.hit_button = Button(ButtonType.HIT, self.pos3_x, self.pos_y)
+        self.stand_button = Button(ButtonType.STAND, self.pos5_x, self.pos_y)
+        self.back_button = Button(ButtonType.BACK, self.pos4_x, self.pos_y)
+        self.quit_button = Button(ButtonType.QUIT, self.pos2_x, self.pos_y)
 
     def __create_scene(self):
         self.screen.fill(TABLE_GREEN)
+        self.screen.blit(self.title_img, self.title_rect)
         if (self.state == GameState.MENU):
-            self.screen.blit(self.title_img, self.title_rect)
             self.quit_button.draw(self.screen)
+            self.start_button.draw(self.screen)
+        if (self.state == GameState.PLAYING):
+            self.hit_button.draw(self.screen)
+            self.back_button.draw(self.screen)
+            self.stand_button.draw(self.screen)
 
     def __handle_action(self, action):
         if action == ButtonType.QUIT:
             self.running = False
+        if action == ButtonType.START:
+            self.state = GameState.PLAYING
+        if action == ButtonType.BACK:
+            self.state = GameState.MENU
 
     def __handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-            self.__handle_action(self.quit_button.handle_event(event))
+            if self.state == GameState.MENU:
+                self.__handle_action(self.start_button.handle_event(event))
+                self.__handle_action(self.quit_button.handle_event(event))
+                
+            elif self.state == GameState.PLAYING:
+                self.__handle_action(self.hit_button.handle_event(event))
+                self.__handle_action(self.stand_button.handle_event(event))
+                self.__handle_action(self.back_button.handle_event(event))
 
     def run(self):
         while self.running:
@@ -74,9 +108,9 @@ class BlackjackGame:
         sys.exit()
 
 class Button:
-    def __init__(self, type, x, y, width=180, height=60):
+    def __init__(self, type, x, y):
         self.type = type
-        self.rect = pygame.Rect(x, y, width, height)
+        self.rect = pygame.Rect(x, y, BUTTON_WIDTH, BUTTON_HEIGHT)
         self.font = pygame.font.Font("assets/casino_font.otf", 32)
         self.color = WOOD_DARK
         self.hover_color = WOOD_LIGHT
